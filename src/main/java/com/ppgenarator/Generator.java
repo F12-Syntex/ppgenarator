286053fa8c88f8d7d071011676b1914b44b66ddc
Saifurrahmaan khan <sk902@kent.ac.uk>
2025-04-12 21:02:21 +0100

    chore(config): create config for pastpapers in/out and ignore data folders

---
+++ src/main/java/com/ppgenarator/Generator.java
@@ -0,0 +1,7 @@
+package com.ppgenarator;
+
+public class Generator {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/ppgenarator/config/Configuration.java
@@ -0,0 +1,8 @@
+package com.ppgenarator.config;
+
+public class Configuration {
+
+    public static final String PAST_PAPER_DIRECTORY = "";
+    public static final String OUTPUT_DIRECTORY = "output";
+
+}
