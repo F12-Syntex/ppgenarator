85f764531ff710cbc25a4e5f9d4e9da9780b1dc2
Saifurrahmaan khan <sk902@kent.ac.uk>
2025-06-15 15:24:52 +0100

    refactor: Simplify getMarks method by removing redundant checks for Paper 3 questions

--- src/main/java/com/ppgenerator/types/Question.java
+++ src/main/java/com/ppgenerator/types/Question.java
@@ -25,51 +25,23 @@ public class Question {
     }
 
     public int getMarks() {
-        // Handle Paper 3 questions 1 and 2 (which have sub-parts like question 6)
-        if (questionNumber.startsWith("question1") || questionNumber.startsWith("question2")) {
-            // These are essay-style questions with variable marks
-            int[] marks = {2, 4, 5, 6, 8, 10, 12, 15, 20, 25};
 
-            for (int mark : marks) {
-                if (questionText != null && questionText.contains("(" + String.valueOf(mark) + ")")) {
-                    return mark;
-                }
+        if (!this.question.getParentFile().getName().contains("3")) {
+            switch (questionNumber) {
+                case "question1":
+                case "question2":
+                case "question3":
+                case "question4":
+                case "question5":
+                    if (this.qualification == Qualification.A_LEVEL) {
+                        return 5;
+                    }
+                    return 4;
             }
-
-            // Default marks for Paper 3 questions 1 and 2 subparts
-            if (questionNumber.contains("a")) {
-                return 2;
-            }
-            if (questionNumber.contains("b")) {
-                return 4;
-            }
-            if (questionNumber.contains("c")) {
-                return 6;
-            }
-            if (questionNumber.contains("d")) {
-                return 8;
-            }
-            if (questionNumber.contains("e")) {
-                return 10;
-            }
-
-            return 5; // Default
-        }
-
-        switch (questionNumber) {
-            case "question1":
-            case "question2":
-            case "question3":
-            case "question4":
-            case "question5":
-                if (this.qualification == Qualification.A_LEVEL) {
-                    return 5;
-                }
-                return 4;
         }
 
         // Handle question 6 and other essay questions
-        int[] marks = {2, 4, 5, 6, 8, 10, 12, 15, 20, 25};
+        int[] marks = { 2, 4, 5, 6, 8, 10, 12, 15, 20, 25 };
 
         for (int mark : marks) {
             if (questionText != null && questionText.contains("(" + String.valueOf(mark) + ")")) {
